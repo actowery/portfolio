5e922264be304aa79f7bf4adea24d80182314313
Cihan Adrian Towery <adrian.towery@gmail.com>
2018-12-03 13:15:33 -0800

    add site to pages

---
+++ program4/keygen.c
@@ -0,0 +1,90 @@
+/******************************************************************************
+ ** Filename:    keygen.c
+ ** Author:      Scott Milton
+ ** Date:        12/7/15
+ **
+ ** Description: This program accepts a whole number from the command line and
+ **              outputs that number of random characters that are either
+ **              capital letters or spaces.
+ **
+ ** Input:       from stdin:    type char*
+ **             
+ ** Output:      to stdout:     type char
+ ******************************************************************************/
+
+
+#include <stdio.h>     // for printf
+#include <stdlib.h>    // for exit, rand, srand
+#include <time.h>      // for time
+
+
+
+/*****************************************************************************
+ ** Function:         randInt()
+ ** Description:      This function generates and returns a psuedo-random
+ **                   number in range from min to max, inclusive.
+ ** Parameters:       two int values: min, max
+ ** Pre-Conditions:   min and max are integer values
+ ** Post-Conditions:  returns the random number
+ **
+ *****************************************************************************/
+int randInt(int min, int max);
+
+
+
+int main(int argc, char** argv)
+{
+
+    char randLetter;
+    int i;
+    int length;
+    time_t sysClock;
+
+    // i.e. - make sure there are enough args
+    if (argc < 2)
+    {
+        printf("Usage: keygen keyLength\n");
+        exit(1);
+    }
+
+    // get length of key to generate from arg #1
+    sscanf(argv[1], "%d", &length);
+
+    if (length < 1)
+    {
+        printf("keygen: invalid keyLength\n");
+        exit(1);
+    }
+
+    // seed the rand() function with system clock
+    srand((unsigned) time(&sysClock));
+
+    // loop length number of times
+    for (i = 0; i < length; i++)
+    {
+        // generate a random letter
+        randLetter = (char) randInt(64, 90);
+
+        // replace asterisk character with space
+        if (randLetter == '@')
+        {
+           randLetter = ' ';
+        }
+
+        printf("%c", randLetter);
+    }
+
+    printf("\n");
+
+    return 0;
+}
+
+
+
+/* uses stdlib.h */
+int randInt(int min, int max)
+{   // returns a random integer between min and max, inclusive
+    // (max - min + 1) is the range
+    // + min sets the bottom of the range
+    return rand() % (max - min + 1) + min;
+}
---
+++ program4/otp_enc_d.c
@@ -0,0 +1,276 @@
+/******************************************************************************
+ ** Filename:    otp_enc_d.c
+ ** Author:      Scott Milton
+ ** Date:        12/7/15
+ **
+ ** Description: This program encrypts a message using modular addition. It 
+ **              receives a plaintext and a key via a socket connection. After
+ **              the encryption is complete it sends the ciphertext back
+ **              through the socket.
+ **
+ ** Input:       from stdin     type char*
+ **              from socket    type char
+ **
+ ** Output:      to socket      type char
+ ******************************************************************************/
+
+#include <fcntl.h>     // for open
+#include <netinet/in.h>
+#include <stdio.h>     // for printf
+#include <stdlib.h>    // for exit
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+#define BUFFER_SIZE    128000
+#define DEBUG          0
+
+
+int main(int argc, char** argv)
+{
+
+    char buffer1[BUFFER_SIZE];
+    char buffer2[BUFFER_SIZE];
+    char buffer3[BUFFER_SIZE];
+    int sockfd;
+    int newsockfd;
+    int i;
+    int keyLength;
+    int numSent;
+    int pid;
+    int plaintextLength;
+    int port;
+    socklen_t clilen;
+
+    struct sockaddr_in server;
+    struct sockaddr_in client;
+
+    // make sure there are enough args
+    if (argc < 2)
+    {
+        printf("Usage: otp_enc_d port\n");
+        exit(1);
+    }
+
+    // validate port number
+    sscanf(argv[1], "%d", &port);
+    if (port < 0 || port > 65535)
+    {
+        printf("otp_enc_d: invalid port\n");
+        exit(2);
+    }
+
+    // create socket
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        printf("Error: opt_enc_d could not create socket\n");
+        exit(1);
+    }
+
+    // zero out the IP address memory space
+    memset(&server, '\0', sizeof(server));
+
+
+    // set up an address
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = INADDR_ANY;
+    server.sin_port = htons(port);
+
+    // bind socket to a port
+    if (bind(sockfd, (struct sockaddr *) &server, sizeof(server)) < 0)
+    {
+        printf("Error: otp_enc_d unable to bind socket to port %d\n", port);
+        exit(2);
+    }
+
+    if (DEBUG)
+    {
+        fflush(stdout);
+        printf("\notp_enc_d: bound socket on port %d\n", port);
+    }
+
+    // listen for connections
+    if (listen(sockfd, 5) == -1)
+    {
+        printf("Error: otp_enc_d unable to listen on port %d\n", port);
+        exit(2);
+    }
+
+    if (DEBUG)
+    {
+        printf("\notp_enc_d: now listening on port %d\n", port);
+    }
+
+    clilen = sizeof(client);
+
+    // accept connections
+    while (1)
+    {
+        newsockfd = accept(sockfd, (struct sockaddr *) &client, &clilen);
+        if (newsockfd < 0)
+        {
+            printf("Error: opt_enc_d unable to accept connection\n");
+            continue;
+        }
+
+        // every time a client connects, spawn off that process
+        pid = fork();
+
+        if (pid < 0)
+        {
+            perror("opt_enc_c: error on fork\n");
+        }
+
+        // child process, do work
+        if (pid == 0)
+        {
+            // do work
+
+            if (DEBUG)
+            {
+                printf("otp_enc_d: connection established with client\n");
+            }
+
+            // make sure not otp_dec_d though!!!
+
+            // zero out buffer
+            memset(buffer1, 0, BUFFER_SIZE);
+
+            // receive plaintext from otp_enc
+
+            plaintextLength = read(newsockfd, buffer1, BUFFER_SIZE);
+            if (plaintextLength < 0)
+            {
+                printf("Error: otp_end_d could not read plaintext on port %d\n", port);
+                exit(2);
+            }
+
+            if (DEBUG)
+            {
+                printf("otp_enc_d: plaintext read: %d characters\n", plaintextLength);
+            }
+
+            // send acknowledgement to client
+            numSent = write(newsockfd, "!", 1);
+            if (numSent < 0)
+            {
+                printf("otp_enc_d error sending acknowledgement to client\n");
+                exit(2);
+            }
+
+            if (DEBUG)
+            {
+                printf("otp_enc_d: acknowledgement sent to client\n");
+            }
+
+            // zero out buffer
+            memset(buffer2, 0, BUFFER_SIZE);
+
+            // receive key from otp_enc
+            keyLength = read(newsockfd, buffer2, BUFFER_SIZE);
+            if (keyLength < 0)
+            {
+                printf("Error: otp_end_d could not read key on port %d\n", port);
+                exit(2);
+            }
+
+            if (DEBUG)
+            {
+                printf("opt_enc_d: key read: %d characters. now processing\n", keyLength);
+            }
+
+            // validate contents of plaintext
+            for (i = 0; i < plaintextLength; i++)
+            {
+                if ((int) buffer1[i] > 90 || ((int) buffer1[i] < 65 && (int) buffer1[i] != 32))
+                {
+                    printf("otp_enc_d error: plaintext contains bad characters\n");
+                    exit(1);
+                }
+            }
+
+            // validate contents of key
+            for (i = 0; i < keyLength; i++)
+            {
+                if ((int) buffer2[i] > 90 || ((int) buffer2[i] < 65 && (int) buffer2[i] != 32))
+                {
+                    printf("otp_enc_d error: key contains bad characters\n");
+                    exit(1);
+                }
+            }
+
+            // compare length of plaintext to that of key
+            if (keyLength < plaintextLength)
+            { 
+                printf("otp_enc_d error: key is too short\n");
+                exit(1);
+            }
+
+            // processing: create ciphertext
+            for (i = 0; i < plaintextLength; i++)
+            {
+                // change spaces to asterisks
+                if (buffer1[i] == ' ')
+                {
+                    buffer1[i] = '@';
+                }
+                if (buffer2[i] == ' ')
+                {
+                    buffer2[i] = '@';
+                }
+
+                // type conversion to int
+                int msg = (int) buffer1[i];
+                int key = (int) buffer2[i];
+
+                // subtract 64 so that range is 0 - 26 (27 characters)
+                msg = msg - 64;
+                key = key - 64;
+
+                // combine key and message using modular addition
+                int cipher = (msg + key) % 27;
+
+                // add 64 back to that range is 64 - 90
+                cipher = cipher + 64;
+
+                // type conversion back to char
+                buffer3[i] = (char) cipher + 0;
+
+                // after encryption, change asterisks to spaces
+                if (buffer3[i] == '@')
+                {
+                    buffer3[i] = ' ';
+                }
+            }
+
+            if (DEBUG)
+            {
+                printf("opt_enc_d: sending response\n");
+            }
+
+            // send ciphertext to otp_enc
+            numSent = write(newsockfd, buffer3, plaintextLength);
+            if (numSent < plaintextLength)
+            {
+                printf("otp_enc_d error writing to socket\n");
+                exit(2);
+            }
+
+            if (DEBUG)
+            {
+                printf("opt_enc_d: response sent\n");
+            }
+
+            // close sockets
+            close(newsockfd);
+            close(sockfd);
+
+            exit(0);
+        }
+
+        else close(newsockfd);
+    } // end while loop
+
+    return 0;
+}
